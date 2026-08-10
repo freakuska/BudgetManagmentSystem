6b3fcdbe3ba24d3815be2984976a93ca406e318a
freakuska <168742983+freakuska@users.noreply.github.com>
2026-08-10 23:09:55 +0300

    Implement token access correctly

--- app/src/main/java/com/example/budget_management_system/models/security/TokenManager.kt
+++ app/src/main/java/com/example/budget_management_system/models/security/TokenManager.kt
@@ -5,7 +5,7 @@ import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.map\nimport kotlinx.coroutines.flow.first\nimport kotlinx.coroutines.runBlocking
 
 private val Context.dataStore by preferencesDataStore(name = "auth_prefs")
 
