852350b0135706cd04339f8b09d54856243f30d7
freakuska <168742983+freakuska@users.noreply.github.com>
2026-08-10 23:08:32 +0300

    Modernize Gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,10 @@ plugins {
     id("com.android.application") version "8.1.1" apply false
     id("com.android.library") version "8.1.1" apply false
     kotlin("android") version "1.9.10" apply false
+    kotlin("kapt") version "1.9.10" apply false
     kotlin("plugin.serialization") version "1.9.10" apply false
 }
 
-task("clean") {
-    delete(rootProject.buildDir)
+tasks.register<Delete>("clean") {
+    delete(layout.buildDirectory)
 }
