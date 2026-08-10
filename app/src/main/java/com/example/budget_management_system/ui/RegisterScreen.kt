438a6d923ab58e52c978d521e84fc4430543bb68
freakuska <168742983+freakuska@users.noreply.github.com>
2026-08-10 23:09:17 +0300

    Fix registration screen import

--- app/src/main/java/com/example/budget_management_system/ui/RegisterScreen.kt
+++ app/src/main/java/com/example/budget_management_system/ui/RegisterScreen.kt
@@ -11,7 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
-import com.example.budget_management_system.viewmodels  .AuthViewModel
+import com.example.budget_management_system.viewmodels.AuthViewModel
 
 @Composable
 fun RegisterScreen(
